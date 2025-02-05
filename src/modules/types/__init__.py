6663aef735f5dfa77ba169cb0902deecb9cb1d0c
aviktorov <aviktorov@astralinux.ru>
2025-02-05 18:42:16 +0300

    added simple types.

---
+++ src/modules/types/__init__.py
@@ -0,0 +1,23 @@
+from .simple_types import (
+    CategoryName,
+    CategoryUID,
+    Description,
+    Email,
+    IncomeUID,
+    MoneySum,
+    OutcomeUID,
+    UserName,
+    UserUID,
+)
+
+__all__ = [
+    "CategoryName",
+    "Description",
+    "Email",
+    "IncomeUID",
+    "MoneySum",
+    "OutcomeUID",
+    "CategoryUID",
+    "UserName",
+    "UserUID",
+]
---
+++ src/modules/types/simple_types.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+
+from pydantic import UUID4
+
+UserUID = Annotated[UUID4, ...]
+CategoryUID = Annotated[UUID4, ...]
+IncomeUID = Annotated[UUID4, ...]
+OutcomeUID = Annotated[UUID4, ...]
+UserName = Annotated[str, ...]
+Email = Annotated[str, ...]
+CategoryName = Annotated[str, ...]
+MoneySum = Annotated[int, ...]
+Description = Annotated[str, ...]
